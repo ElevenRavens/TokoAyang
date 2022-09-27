3f597f970a7d6f26310420430a6050c7a212c5b0
LiviaDita09 <liviadita123@gmail.com>
2022-09-27 17:13:36 +0700

    project tokoku

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\LatihanController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return "Hallo nama saya Moch Rizal Hermawan";
+});
+
+Route::get('/latihan', [LatihanController::class, 'index']);
+
+Route::get('/beranda', [LatihanController::class, 'beranda']);
+
+Route::get('/', [HomepageController::class, 'index']);
